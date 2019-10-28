4d616ba0eba4baf917726235bbe7faad7bd2f70b
tauheedkhan <tauheedkhan@outlook.com>
2019-10-28 11:44:18 +0000

    fix

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log('request:: ', req)
+  //  res.send(JSON.parse(req))
+    res.json(req.headers)
+})
+
+app.post('/', (req, res) => {
+    res.json(req.headers)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
